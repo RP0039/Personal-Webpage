29c579fe8a4b11f6b40bc53f8230051e426b83b5
RP0039 <RiveroVal3984@gmail.com>
2026-03-11 12:55:50 -0300

    Add files via upload

---
+++ html/post/style_js/app.js
@@ -0,0 +1,16 @@
+
+//  ---- MODO OCURO ----  //
+let toggle=document.getElementById('toggle');
+let label_toggle=document.getElementById('label_toggle');
+toggle.addEventListener('change',(event)=>{
+    let checked=event.target.checked;
+    document.body.classList.toggle('dark');
+    if (checked==true){
+        label_toggle.innerHTML='<i class="fa-solid fa-sun"></i>';
+        label_toggle.style.color="white";
+    }else{
+        label_toggle.innerHTML='<i class="fa-solid fa-moon"></i>';
+        label_toggle.style.color="black"
+    }
+})
+
