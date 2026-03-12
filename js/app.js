dd5011bb119cc02fd7b7d20e2b4086d60f5e67b9
RP0039 <RiveroVal3984@gmail.com>
2026-03-12 19:08:23 -0300

    Improved dark and light mode support.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
 
 //  ---- MODO OCURO ----  //
-let toggle=document.getElementById('toggle');
-let label_toggle=document.getElementById('label_toggle');
-toggle.addEventListener('change',(event)=>{
-    let checked=event.target.checked;
+
+let toggle = document.getElementById('toggle');
+let label_toggle = document.getElementById('label_toggle');
+
+// aplicar tema guardado
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    toggle.checked = true;
+    label_toggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    label_toggle.style.color = "white";
+}
+
+toggle.addEventListener('change', (event) => {
+
+    let checked = event.target.checked;
+
     document.body.classList.toggle('dark');
-    if (checked==true){
-        label_toggle.innerHTML='<i class="fa-solid fa-sun"></i>';
-        label_toggle.style.color="white";
-    }else{
-        label_toggle.innerHTML='<i class="fa-solid fa-moon"></i>';
-        label_toggle.style.color="black"
+
+    if (checked) {
+        label_toggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        label_toggle.style.color = "white";
+
+        localStorage.setItem("theme", "dark");
+    } else {
+        label_toggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        label_toggle.style.color = "black";
+
+        localStorage.setItem("theme", "light");
     }
-})
+
+});
