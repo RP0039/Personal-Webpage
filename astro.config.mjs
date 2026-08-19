51069a36f1524b5f2c6f40ea66c0fc602568080f
rp0039 <riveroval3984@gmail.com>
2026-08-19 15:40:35 -0300

    style: refresh global design and homepage layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+        defaultLocale: 'es',
+        locales: ['es', 'en'],
+        routing: {
+            prefixDefaultLocale: true,
+        }
+    },
   vite: {
     plugins: [tailwindcss()],
   }
